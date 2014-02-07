7f4a5032a2fdcd933575c719f0bcadf8f99806d2
Sam Moore <matches@ucc.asn.au>
2014-02-07 21:55:11 +0800

    Movement, Boxes, Collisions, and Splash Screens
    The biggest complaint about HtR I used to get was that it was awful*.
    
    The second biggest complaint was that it was disconcerting that the game simply restarted
    when you died, with no indication of what had happened.
    
    So, I present to you <drumroll> *Splash* Screens! With glorious SVGs!
    The SVGs may need some resizing/recolouring work. But I doubt that work will happen.
    
    The splash screens do tend to occasionally cause the whole browser to crash,
    but that should only be a minor inconvenience, as no one should be using their browser
    for anything else anyway.
    
    I did some other stuff, but that pales in comparison to the glory of the Splash screens.
    
    Next: Foxes! Music! Profit? Mwuhaha
    
    * Although some people did actually say it was awful in a good way...
    
    Gah, why is my EDITOR nano

--- scene.js
+++ scene.js
@@ -30,6 +30,7 @@ var aTextureCoord;
 /** GL Vertex Shader Uniforms **/
 var uPosition;
 var uAspectRatio;
+var uColour;
 
 /** GL Buffers **/
 var gVerticesBuffer;
@@ -38,6 +39,12 @@ var gVerticesIndexBuffer;
 
 /** Other globals **/
 var stepRate = 15;
+var keysPressed = [];
+var player;
+var gravity = 1.0;
+
+var drawSceneTimer;
+var addBoxTimer;
 
 /**
  * Debug; display information on the page (for most things this is much nicer than Alt-Tabbing to and fro with Firebug)
@@ -58,6 +65,15 @@ function Entity(position, velocity)
 {
 	this.position = position;
 	this.velocity = velocity;
+	this.lastPosition = []; for (var i in this.position) this.lastPosition[i] = this.position[i];
+}
+
+/**
+ * Name of Entity
+ */
+Entity.prototype.GetName = function()
+{
+	return (this.name) ? this.name : "unnamed";
 }
 
 /**
@@ -72,39 +88,44 @@ Entity.prototype.Step = function()
 		this.lastUpdateTime = currentTime;
 		return;
 	}
-	var delta = currentTime - this.lastUpdateTime;
+	this.delta = currentTime - this.lastUpdateTime;
 
-	this.lastPosition = []; for (var i in this.position) this.lastPosition[i] = this.position[i];
+	this.lastPosition = [];
+
+	// Deal with keyboard state
+	if (this.handleKeys)
+		this.handleKeys(keysPressed);
 
 	// Update velocity
 	if (this.acceleration)
 	{
 		for (var i in this.velocity)
-			this.velocity[i] += delta * this.acceleration[i] / 1000;
+			this.velocity[i] += this.delta * this.acceleration[i] / 1000;
 	}
 
 	// Update position
 	for (var i in this.position)
-		this.position[i] += delta * this.velocity[i] / 1000;
-
-	// Check for collisions
-	if (this.bounds)
 	{
-		for (var i in gEntities)
+		if (this.velocity[i] == 0) continue;
+		this.lastPosition[i] = this.position[i];
+		this.position[i] += this.delta * this.velocity[i] / 1000;
+
+		// Check for collisions
+		collide = this.Collision();
+		if (collide)
 		{
-			if (gEntities[i] != this) this.HandleCollision(gEntities[i]);
+				// Soo... this is terribly inefficient and lazy
+				// I can't be bothered looking up vector maths
+				// (And it seems I couldn't last time I wrote HtR)
+				this.position[i] = this.lastPosition[i];
+				while (!this.Collides(collide))
+					this.position[i] += this.delta * 0.05 * this.velocity[i] / 1000;
+				this.position[i] -= this.delta * 0.05 * this.velocity[i] / 1000;
+				if (this.HandleCollision(collide,true))
+					this.velocity[i] = 0;
 		}
 	}
 
-	// Check object in bounds.
-	if (this.position[0] < -1)
-		this.Bounce([1,0]);
-	else if (this.position[0] > 1)
-		this.Bounce([-1,0]);
-	else if (this.position[1] < -1)
-		this.Bounce([0,1]);
-	else if (this.position[1] > 1)
-		this.Bounce([0,-1]);
 
 	// Change sprite based on direction
 	if (this.frameBase)
@@ -136,7 +157,7 @@ Entity.prototype.Step = function()
 	if (this.frames && this.frameRate)
 	{
 		if (!this.frameNumber) this.frameNumber = 0;
-		this.frameNumber += delta * this.frameRate / 1000;
+		this.frameNumber += this.delta * this.frameRate / 1000;
 		this.frame = this.frames[Math.floor(this.frameNumber) % this.frames.length]
 	}
 
@@ -144,8 +165,48 @@ Entity.prototype.Step = function()
 	this.lastUpdateTime = currentTime;
 	for (var i in this.position) this.lastPosition[i] = this.position[i];
 
-	Debug("<p>Position: ["+this.position+"]</p><p>Velocity: ["+this.velocity+"]</p>",true);
-	Debug("<p>FrameNum: "+this.frameNumber+"</p>");
+	if (this.debug)
+	{
+		Debug("<p>Position: ["+this.position+"]</p><p>Velocity: ["+this.velocity+"]</p>",true);
+		Debug("<p>FrameNum: "+this.frameNumber+"</p>");
+	}
+}
+
+Entity.prototype.Top = function() {return this.position[1] + this.bounds.max[1];}
+Entity.prototype.Bottom = function() {return this.position[1] + this.bounds.min[1];}
+Entity.prototype.Left = function() {return this.position[0] + this.bounds.min[0];}
+Entity.prototype.Right = function() {return this.position[0] + this.bounds.max[0];}
+
+/**
+ * Get the actual bounding box
+ */
+Entity.prototype.GetBoundBox = function()
+{
+	var box = { min : [], max : [] };
+	for (var i in this.position)
+	{
+		box.min[i] = this.position[i];
+		box.max[i] = this.position[i];
+		if (this.bounds)
+		{
+			box.min[i] += this.bounds.min[i];
+			box.max[i] += this.bounds.max[i];
+		}
+	}
+	return box;
+}
+
+
+/**
+ * Find Object that collides with this one
+ */
+Entity.prototype.Collision = function()
+{
+	for (var i in gEntities)
+	{
+		if (gEntities[i] === this) continue;
+		if (this.Collides(gEntities[i])) return gEntities[i];
+	}
 }
 
 /**
@@ -154,46 +215,57 @@ Entity.prototype.Step = function()
 Entity.prototype.Collides = function(other)
 {
 	if (!other.bounds || !this.bounds) return;
+
+
+	var A = this.GetBoundBox();
+	var B = other.GetBoundBox();
+
+	var collides = true;
 	for (var i in this.position)
 	{
-		alert("unimplemented");
+		collides &= (A.min[i] <= B.max[i] && A.max[i] >= B.min[i]);
 	}
-	
+	return collides;
 }
 
 /**
- * Handle collision
+ * Deal with a collision
  */
-Entity.prototype.HandleCollision = function(other)
+Entity.prototype.HandleCollision = function(other, instigator)
 {
-	var n = this.Collides(other);
-	if (n)
-	{
-		this.Bounce(n);
-		other.Bounce(n);
-	}
-	return n;
+	
+	if (instigator)
+		other.HandleCollision(this, false);
+	if (typeof(this.canJump) !== "undefined") this.canJump = true;
+	return true;
 }
+
 
 /**
  * Bounce off a surface with normal vector n
  */
-Entity.prototype.Bounce = function(n)	
+Entity.prototype.Bounce = function(n, reflect)	
 {
 	if (this.lastPosition)
 	{
 		for (var i in this.lastPosition) this.position[i] = this.lastPosition[i];
 	}
-        var dot = 0;
-        for (var j in this.velocity)
+    
+	var dot = 0;
+	for (var j in this.velocity)
 	{
-                dot += this.velocity[j] * n[j];
+    	dot += this.velocity[j] * n[j];
 	}
 
-        for (var j in this.velocity)
-        {
-                this.velocity[j] = - (2.0 * dot * n[j] - this.velocity[j]);
-        }
+//	alert("Bounce normal ["+n+"] , v ["+this.velocity+"] , dot "+dot);
+    for (var j in this.velocity)
+    {
+		this.velocity[j] -= dot*n[j]; // zero in direction of normal only.
+		this.position[j] += this.delta * this.velocity[j] / 1000;
+		//this.lastPosition[j] = this.position[j];
+	}
+//	alert("velocity: ["+this.velocity+"]");
+	this.canJump = (typeof(this.canJump) !== "undefined")
 }
 
 
@@ -217,8 +289,15 @@ Entity.prototype.Draw = function()
 	gl.bindBuffer(gl.ARRAY_BUFFER, gVerticesTextureCoordBuffer);
 	gl.vertexAttribPointer(aTextureCoord, 2, gl.FLOAT, false, 0, 0);
 	gl.activeTexture(gl.TEXTURE0);
-	gl.bindTexture(gl.TEXTURE_2D, this.frame);
+	if (this.frame.tex)
+		gl.bindTexture(gl.TEXTURE_2D, this.frame.tex);
 	gl.uniform1i(gl.getUniformLocation(shaderProgram, "uSampler"), 0);
+
+	// Set the scale of this object
+	if (this.scale)
+		gl.uniform2f(uScale, this.scale[0],this.scale[1]);
+	else if (this.frame.img)
+		gl.uniform2f(uScale, this.frame.img.width/canvas.width, this.frame.img.height/canvas.height);
 
 	// Set vertex indices and then draw the rectangle
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gVerticesIndexBuffer);
@@ -246,6 +325,158 @@ Entity.prototype.LoadSprites = function(imageDir)
 }
 
 
+function BoxStep()
+{
+	Entity.prototype.Step.call(this);
+	if (this.velocity[0] < 1e-2)
+		this.velocity[0] = 0;
+	else
+		this.velocity[0] /= 4;
+}
+
+/**
+ * Add a Box
+ */
+function AddBox()
+{
+	Debug("Box Time!", true);
+	setTimeout(function() {Debug("",true)}, 200*stepRate);
+	var box = new Entity([player.position[0],1],[0,0]);
+	box.frame = LoadTexture("data/box/box1.gif");
+	box.acceleration = [0,-gravity];
+	box.bounds = {min: [-32/canvas.width, -32/canvas.height], max: [32/canvas.width, 32/canvas.height]};
+	box.name = "Box";
+
+	box.Step = BoxStep;
+	gEntities.push(box);
+
+}
+
+/**
+ * Load the initial Entities
+ */
+function LoadEntities()
+{
+	gEntities = [];
+	
+	// The player (Humphrey The Rabbit)
+	player = new Entity([0,0],[0,0]);
+	player.LoadSprites("data/rabbit");
+	player.bounds = {min: [-32/canvas.width, -32/canvas.height], max: [32/canvas.width, 32/canvas.height]};
+	player.frameRate = 3;
+	player.canJump = true;
+	player.acceleration = [0, -gravity]; // gravity
+	// Handle any keys pressed
+	player.handleKeys = function(keys)
+	{
+		this.velocity[0] = 0;
+		if (keys[37]) this.velocity[0] -= 0.5;
+		if (keys[39]) this.velocity[0] += 0.5;
+		if (this.canJump && keys[38])
+		{
+			this.velocity[1] = 1;
+			this.canJump = false;
+		}
+	}
+	// Handle collision with box
+	player.HandleCollision = function(other, instigator)
+	{
+		if (!instigator && other.velocity[1] < -1e-2)
+		{
+			Debug("YOU GOT SQUISHED!", true);
+			PauseGame();
+			DeathScreen();
+			setTimeout(StartGame,2000);
+		}
+		else if (this.Bottom() < other.Top() && this.Top() > other.Bottom())
+		{
+			other.velocity[0] = this.velocity[0] / 2;
+			// Behold... inheritence implemented in JavaScript (Yuk)
+			Entity.prototype.HandleCollision.call(this,other, instigator);
+			return false;
+		}
+		return Entity.prototype.HandleCollision.call(this,other, instigator);
+	}
+	player.name = "Humphrey";
+
+	var ground = new Entity([-1,-1],[0,0]);
+	ground.bounds = {min: [-Infinity,-Infinity], max:[Infinity, 0]};
+	ground.Bounce = function() {};
+	ground.name = "Ground";
+
+	var leftWall = new Entity([-1,1],[0,0]);
+	leftWall.bounds = {min: [-Infinity,-Infinity], max:[0,Infinity]};
+	leftWall.Bounce = function() {};
+	leftWall.name = "LeftWall";
+
+	var rightWall = new Entity([1,1],[0,0]);
+	rightWall.bounds = {min: [0,-Infinity], max:[Infinity,Infinity]};
+	rightWall.Bounce = function() {};
+	rightWall.name = "RightWall";
+
+	
+
+	
+	gEntities.push(player);
+	gEntities.push(ground);
+	gEntities.push(leftWall);
+	gEntities.push(rightWall);
+}
+
+function DeathScreen() {SplashScreen([0,0,0,1],[1,0,0,1], "DEATH")}
+function StartScreen() {SplashScreen([0.9,0.9,0.9,1],[1,1,1,1], "Humphrey The Rabbit")}
+
+function SplashScreen(background, blend, text)
+{
+	if (drawSceneTimer) clearTimeout(drawSceneTimer);
+	if (addBoxTimer) clearTimeout(addBoxTimer);
+
+
+	var screen = new Entity([0,0], [0,0]);
+	var X = Math.round(1 + (Math.random()*6));
+	screen.frame = LoadTexture("data/rabbit/drawing"+X+".svg", function() {
+		gl.clearColor(background[0], background[1], background[2], background[3]);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+		var ratio = (screen.frame.img.width / screen.frame.img.height);
+		screen.scale = [0.6*ratio, 0.6*ratio]; 
+		gl.uniform4f(uColour, blend[0], blend[1], blend[2], blend[3]);
+		screen.Draw();
+		gl.uniform4f(uColour,1,1,1,1); 
+		Debug("<p align=center><b><i>"+text+"</i></b></p>", true);
+	});
+}
+
+/**
+ * Start the game
+ */
+function StartGame()
+{
+	if (drawSceneTimer) clearTimeout(drawSceneTimer);
+	if (addBoxTimer) clearTimeout(addBoxTimer);
+
+	gl.clearColor(0.9,0.9,1,1);
+
+	Debug("GET READY!",true);
+	setTimeout(function() {Debug("",true)}, stepRate*500);
+	// Load entities
+	LoadEntities();
+	// Draw the Scene every stepRate ms	
+	drawSceneTimer = setInterval(DrawScene, stepRate);
+	// Drop a box every 1000 steps
+	addBoxTimer = setInterval(AddBox, stepRate*1000);
+}
+
+/**
+ * Pause the game
+ */
+function PauseGame()
+{
+	if (drawSceneTimer) clearTimeout(drawSceneTimer);
+	if (addBoxTimer) clearTimeout(addBoxTimer);
+
+}
+
+
 /**
  * The main function
  */
@@ -254,6 +485,8 @@ function main()
 	canvas = document.getElementById("glcanvas");
 	InitWebGL(canvas);      // Initialize the GL context
 
+	gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+	gl.enable(gl.BLEND);
 	gl.clearColor(0.9, 0.9, 1.0, 1.0); // Set clear colour 
 	gl.viewport(0,0,canvas.width,canvas.height); // Set viewport (unnecessary?)
 	//gl.disable(gl.DEPTH_TEST); // No point using depth testing for 2D graphics
@@ -266,24 +499,15 @@ function main()
     	// Initialize the shaders; this is where all the lighting for the
 	// vertices and so forth is established.
 	InitShaders();
-	
-	gEntities = [];
 
-	var rabbit = new Entity([0,0],[0.5,0]);
-	rabbit.LoadSprites("data/rabbit");
-	
-//	rabbit.frame = rabbit.frames[0]; 
-	rabbit.frameRate = 3;
-	rabbit.acceleration = [0,0.1];
+	// Keyboards
+	document.onkeydown = function(event) {keysPressed[event.keyCode] = true};
+	document.onkeyup = function(event) {keysPressed[event.keyCode] = false};
 
-	rabbit.bounds = {min : [0,0], max : [1,1]};
-
-	//rabbit.Step = function() {Entity.prototype.Step(); Debug("Rabbit step", true);};
-
-	gEntities[gEntities.length] = rabbit;
-
-	// Draw the Scene every stepRate ms	
-	setInterval(DrawScene, stepRate);
+	// Start the Game.
+//	StartGame();
+	StartScreen();
+	setTimeout(StartGame, 2000);
 }
 
 /**
@@ -291,6 +515,7 @@ function main()
  */
 function DrawScene()
 {
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	for (var i in gEntities)
 	{
 		gEntities[i].Step();
@@ -320,13 +545,16 @@ function InitWebGL()
 /**
  * Load textures
  */
-function LoadTexture(src)
+function LoadTexture(src, lambda)
 {
 	var texture = gl.createTexture();
 	var image = new Image();
 	image.src = src; 
-	image.onload = function() {HandleTextureLoaded(image, texture);}
-	return texture;
+	if (lambda)
+		image.onload = function() {HandleTextureLoaded(image, texture); lambda()};
+	else
+		image.onload = function() {HandleTextureLoaded(image, texture)};
+	return {tex : texture, img : image};
 }
 
 
@@ -337,9 +565,35 @@ function LoadTexture(src)
 function HandleTextureLoaded(image, texture)
 {
 	gl.bindTexture(gl.TEXTURE_2D, texture);
+
+	if ((image.width & (image.width - 1)) != 0 || (image.height & (image.height - 1)) != 0)
+	{
+		var canvas = document.createElement("canvas");
+		var w = image.width; var h = image.height;
+		--w; for (var i = 1; i < 32; i <<= 1) w = w | w >> i; ++w;
+		--h; for (var i = 1; i < 32; i <<= 1) h = h | h >> i; ++h;
+		canvas.width = w;
+		canvas.height = h;
+		var ctx = canvas.getContext("2d");
+		
+		ctx.drawImage(image, w/2 - image.width/2, h/2 - image.height/2, image.width, image.height);
+		//ctx.font = "30px Courier";
+		//ctx.fillText("hello world\nhow are you", 0.1*w, h/2, 0.8*w);
+		/*
+		ctx.beginPath();
+		ctx.moveTo(0,0);
+		ctx.lineTo(w,0);
+		ctx.lineTo(w,h);
+		ctx.lineTo(0,h);
+		ctx.lineTo(0,0);
+		ctx.stroke();
+		*/
+		image = canvas;
+	}
+
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
 	gl.generateMipmap(gl.TEXTURE_2D);
 	gl.bindTexture(gl.TEXTURE_2D, null);
 }
@@ -408,9 +662,11 @@ function InitShaders()
 	uPosition = gl.getUniformLocation(shaderProgram, "uPosition");
 	uAspectRatio = gl.getUniformLocation(shaderProgram, "uAspectRatio");
 	uScale = gl.getUniformLocation(shaderProgram, "uScale");
-	gl.uniform1f(uScale, 32/640);
+	uColour = gl.getUniformLocation(shaderProgram, "uColour");
+	gl.uniform4f(uColour, 1,1,1,1);
 	// Set it
-	gl.uniform1f(uAspectRatio, canvas.width/canvas.height);
+	//gl.uniform1f(uAspectRatio, canvas.width/canvas.height);
+	gl.uniform1f(uAspectRatio, 1);
 }
 
 //
