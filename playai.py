2e173057e8eb2ba1dc2cd3dac2791a21d57e0715
Sam Moore <matches@ucc.asn.au>
2014-02-09 01:56:45 +0800

    Fix playai.py
    SOMEHOW I deleted most of it... thank goodness for vim backup files.

--- playai.py
+++ playai.py
@@ -7,6 +7,48 @@ import cgi
 #cgitb.enable()
 
 
+HTML_TEMPLATE = """
+	<html>
+		<head>
+			<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+			<meta content="utf-8" http-equiv="encoding">
+			<title>HtR AI Upload</title>
+		</head>
+		<body>
+			<h1>HtR Customised AI</h1>
+			<p> You can upload a function that will automatically play <a href="index.html">HtR</a> here. </p>
+			<p></p>
+			<p><b>Upload Text:</b></p>
+			<textarea rows="20" cols="60" name="script" form="form">
+/**
+ * Implement this function to make a customised Humphrey
+ * player - {x, y, vx, vy, ...} - Where the player is
+ * foxes - [{x, y, vx, vy, ...}, ...] - All the foxes
+ * boxes - [{x, y, vx, vy, ...}, ...] - All the boxes
+ * Return true or false for {up, down, left, right}
+ */
+function AIStep(player, foxes, boxes)
+{
+    /** Sample - randomly press keys **/	
+    var result = {
+        up : (Math.random() > 0.5),
+        down : (Math.random() > 0.5),
+        left : (Math.random() > 0.5),
+        right : (Math.random() > 0.5)
+    };
+    return result;
+}</textarea>
+			<form action="%(SCRIPT_NAME)s" method="POST"" id="form" enctype="multipart/form-data">
+				<p> <b>Upload a File:</b> <input type="file" name="file"> <button type="reset">Reset</button> </p>
+				<p> (Above text will be ignored if file is supplied)</p>
+				<p> Or don't be lazy and just clone the <a href="https://github.com/szmoore/htr-webgl">GitHub repo</a></p>
+				<input type="submit"> 
+			</form>
+			
+		</body>
+	</html>
+"""
+
 def print_html_form():
 	print "content-type:text/html\n"
 	print HTML_TEMPLATE % {"SCRIPT_NAME" : os.environ["SCRIPT_NAME"]}
