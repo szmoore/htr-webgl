5eb45986315a2175063432e1dea1c4cd9b8b6bf5
Sam Moore <matches@ucc.asn.au>
2014-02-15 04:56:24 +0800

    Improve page layout - add side panels
    Side panels can be used to display content (eg: advertisments) but can be hidden/shown by the user.
    They will also resize and disappear if the window isn't big enough.
    
    The middle pannel with the game takes priority. Everything else is resized or removed.
    If the window gets smaller than 640x480 there isn't much we can do? Zooming out in the browser should help?
    
    Made information bar and made status bar smaller.
    
    I'm not sure whether I'm allowed to actually put ads in the side panels.
    Or whether I want to.
    
    But the panels can be used for other stuff, and I've learned how to use <div> (sort of).

---
+++ page.js
@@ -0,0 +1,82 @@
+function SetCookie(cname,cvalue,exdays)
+{
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function GetCookie(cname)
+{
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++)
+	{
+		var c = ca[i].trim();
+		if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+	}
+	return "";
+}  
+
+var adsShown = (GetCookie("adblock") == "true");
+var adIds = {"leftPanel" : null, "rightPanel" : null};
+
+function InitPage()
+{
+	var w = parseInt(window.innerWidth);
+	var h = parseInt(window.innerHeight);
+	var minW = parseInt(document.getElementById("middlePanel").style.width.replace("px",""));
+
+	var adWidth = (w/2 - 6*minW/10);
+
+	var hideads = document.getElementById("hideads");
+
+	for (var a in adIds)
+	{
+		if (!adIds[a])
+			adIds[a] = document.getElementById(a);
+		adIds[a].style.display = (adsShown) ? "block" : "none";
+		if (adsShown)
+		{
+			adIds[a].style.width = ""+adWidth+"px";
+			if (adWidth <= 0)
+			{
+				adIds[a].style.display = "none";
+				hideads.color = "transparent"; //hideads.onclick = function() {};
+			}
+			else
+			{
+				hideads.color = "black"; //hideads.onclick = HideAds;
+			}
+		}
+			
+	}
+	adIds["rightPanel"].innerHTML = "<iframe src=\"right_advert.html\" height=\"500px\" width=\""+adWidth+"px\" frameBorder=\"0\"></iframe>";
+
+	document.getElementById("hideads").innerHTML = (adsShown) ? "[Hide Advertisments]" : "[Show Advertisements]";
+	document.getElementById("statusBar").style.display = (h > 520) ? "block" : "none";
+	document.getElementById("infoBar").style.display = (h > 520) ? "block" : "none";
+
+}
+
+function HideAds()
+{
+	if (adsShown)
+	{
+		if (GetCookie("adblock") == "")
+		{
+			alert("Fair enough! I don't like Ads either!\nThey won't bother you again.\n\nBy the way, have you heard of Ad Block?\nhttp://google.com#q=ad+block");
+		}
+		SetCookie("adblock","true",3650);
+		adsShown = false;
+	}
+	else
+	{
+		adsShown = (GetCookie("adblock") != "" || confirm("Wait what? You want to see ads?"));
+		if (adsShown)
+			SetCookie("adblock", "false",3650);
+		else
+			SetCookie("adblock", "true",3650);
+	}
+	InitPage();
+}
