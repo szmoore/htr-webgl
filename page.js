c39b4573758208554d67547f38755a6f8b7bc78f
Sam Moore <matches@ucc.asn.au>
2014-07-23 12:24:39 +0800

    Enable external advertisements again
    Yeah I'm an evil capitalist bastard and all that.
    
    If you set the "adblock" cookie to something other than "yes"
    you will hide the external advert. If you set it to "yes" it hides
    the internal adverts (the ones that I wrote myself) as well.
    
    You should also be able to resize the screen to hide it.
    
    External advert is not under git for obvious reasons.

--- page.js
+++ page.js
@@ -1,12 +1,30 @@
-
 function InitPage()
 {	
-	var width = Math.min(0.9*window.screen.width, 640);
-	var height = Math.min(0.7*window.screen.height, 800);
+	// Browsers on phones do *wierd* things with screen.width, innerWidth, screen.availWidth etc
+	var screenWidth = Math.min(window.innerWidth, window.screen.width);
+	var screenHeight = Math.min(window.innerHeight, window.screen.height);
+	var width = Math.min(0.9*screenWidth, 640);
+	var height = Math.min(screenHeight-64, 800);
+	
+	var middlePanel = document.getElementById("middlePanel");
+	middlePanel.style.width = width;
 	
 	var canvas = document.getElementById("glcanvas");
 	canvas.width = width;
 	canvas.height = height;
+	
+	// The external advertisement
+	var banner = document.getElementById("banner");
+	if (banner)
+	{
+		banner.style.height = height;
+		banner.style.width = Math.max((0.9*screenWidth - width)/2, 0);
+		if (banner.style.width < 50 || GetCookie("adblock"))
+			banner.style.display = "none";
+		else
+			banner.style.display = "block";
+	}
+
 	var loading = document.getElementById("loading");
 	if (loading)
 		loading.parentNode.removeChild(loading);
